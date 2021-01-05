f06a4410b739711c9a993e599fc1b43d5297eb96
AmySmileBatto <74995093+AmySmileBatto@users.noreply.github.com>
2021-01-05 09:59:38 -0500

    Removed weird head and duplication

--- VirtualHank2/VirtualHank/Interpreter.h
+++ VirtualHank2/VirtualHank/Interpreter.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #pragma once
 #include <fstream>
 #include <vector>
@@ -7,13 +6,3 @@
 extern int g_version;
 
 int InterpretFile(const char*);
-=======
-#pragma once
-#include <fstream>
-#include <vector>
-#include "Functions.h"
-
-extern int g_version;
-
-int InterpretFile(const char*);
->>>>>>> 0641cdd506e8bd139e5868b54fb5c1396d32d3b8
