b854584c63e86f784b6c4c4930449b21d3309a44
Amy <henrythepony@gmail.com>
2021-01-17 13:16:14 -0500

    Started working on loading saved diagrams - see TODOs

--- ElectronArchitect/RaylibApp/Serialization.cpp
+++ ElectronArchitect/RaylibApp/Serialization.cpp
@@ -26,12 +26,11 @@ void Save(std::vector<Transistor*>* fromArr)
 		char transistorTypeSymbol;
 		switch (transistor->type)
 		{
-		case Transistor::Type::Invert:
-			transistorTypeSymbol = '!';
-			break;
-
 		default: case Transistor::Type::Simple:
 			transistorTypeSymbol = '|';
+			break;
+		case Transistor::Type::Invert:
+			transistorTypeSymbol = '!';
 			break;
 		}
 		saveFile << transistorTypeSymbol << " ";
@@ -110,95 +109,126 @@ void Load(std::vector<Transistor*>* toArr, std::vector<Wire*>* wireArr)
 {
 	std::ifstream loadFile("saved_diagram.txt", std::ifstream::in);
 	std::vector<Transistor*> transistors;
+	std::vector<Wire*> wires;
 	std::vector<size_t> linkIndexes;
 
-	loadFile.ignore(256, 's');
-	size_t size = 0;
-	loadFile >> size;
-	while (loadFile.tellg() < size)
-	{
-		Transistor* transistor = new Transistor;
-		transistors.push_back(transistor);
+	// Get file size
+	//loadFile.seekg(0, std::ifstream::end);
+	//std::streampos fileSize = loadFile.tellg();
+	//loadFile.seekg(0, std::ifstream::beg);
 
-		loadFile.ignore(256,'t');
+	// Get number of transistors
+	loadFile.ignore(256, 's');
+	size_t transistorCount = 0;
+	loadFile >> transistorCount;
+
+	for (int i = 0; i < transistorCount; ++i)
+	{
+		Transistor* transistor = new Transistor();
+		transistors.push_back(transistor);
+	}
+
+	for (Transistor* transistor : transistors)
+	{
+		loadFile.ignore(256, 't');
+		
 		loadFile >> transistor->pos.x >> transistor->pos.y;
 
 		char transistorTypeSymbol;
 		loadFile >> transistorTypeSymbol;
 		switch (transistorTypeSymbol)
 		{
-		case '!':
-			transistor->type = Transistor::Type::Invert;
-			break;
-
 		default: case '|':
 			transistor->type = Transistor::Type::Simple;
 			break;
+		case '!':
+			transistor->type = Transistor::Type::Invert;
+			break;
+		}
+
+		size_t inputCount = 0;
+		loadFile >> inputCount;
+		for (size_t i = 0; i < inputCount; ++i)
+		{
+			size_t inputTransistorIndex = 0;
+			loadFile >> inputTransistorIndex;
+
+			// TODO: Think through how this is going actually work sequencially. When and how will the wires be initialized and how can we know whether one is already connecting to us?
+			Wire* newWire = nullptr;
+			for (Wire* wire : transistors[inputTransistorIndex]->outputs)
+			{
+				if (wire->outTransistor == transistor) // Searching for self as output to wire
+				{
+					newWire = wire;
+					break;
+				}
+			}
+			if (!newWire) // If an existing wire was not found, create one.
+			{
+				newWire = new Wire();
+				newWire->inTransistor = transistors[inputTransistorIndex];
+				newWire->outTransistor = transistor;
+				transistor->inputs.push_back(newWire);
+			}
+
+			// TODO: Probably not necessary if the wire has already been previously initialized!
+			char wireDirectionSymbol;
+			loadFile >> wireDirectionSymbol;
+			switch (wireDirectionSymbol)
+			{
+			default: case 'x':
+				newWire->direction = WireDirection::XFirst;
+				break;
+			case 'y':
+				newWire->direction = WireDirection::YFirst;
+				break;
+			case 'd':
+				newWire->direction = WireDirection::Direct;
+				break;
+			}
 		}
 
 		// TODO
+		size_t outputCount = 0;
+		loadFile >> outputCount;
+		for (size_t i = 0; i < outputCount; ++i)
+		{
+			size_t outputTransistorIndex = 0;
+			loadFile >> outputTransistorIndex;
 
-		//for (Wire* inputWire : transistor->inputs)
-		//{
-		//	saveFile << transistor->inputs.size() << " ";
-		//	for (size_t searchIndex = 0; searchIndex < fromArr->size(); ++searchIndex)
-		//	{
-		//		if (fromArr->at(searchIndex) == inputWire->inTransistor)
-		//		{
-		//			saveFile << searchIndex << " ";
-		//
-		//			char directionSymbol;
-		//			switch (inputWire->direction)
-		//			{
-		//			default: case WireDirection::XFirst:
-		//				directionSymbol = 'x';
-		//				break;
-		//
-		//			case WireDirection::YFirst:
-		//				directionSymbol = 'y';
-		//				break;
-		//
-		//			case WireDirection::Direct:
-		//				directionSymbol = 'd';
-		//				break;
-		//			}
-		//			saveFile << directionSymbol << " ";
-		//
-		//			break; // Break out of search, not input array
-		//		}
-		//	}
-		//}
-		//
-		//for (Wire* outputWire : transistor->outputs)
-		//{
-		//	saveFile << transistor->outputs.size() << " ";
-		//	for (size_t searchIndex = 0; searchIndex < fromArr->size(); ++searchIndex)
-		//	{
-		//		if (fromArr->at(searchIndex) == outputWire->outTransistor)
-		//		{
-		//			saveFile << searchIndex << " ";
-		//
-		//			char directionSymbol;
-		//			switch (outputWire->direction)
-		//			{
-		//			default: case WireDirection::XFirst:
-		//				directionSymbol = 'x';
-		//				break;
-		//
-		//			case WireDirection::YFirst:
-		//				directionSymbol = 'y';
-		//				break;
-		//
-		//			case WireDirection::Direct:
-		//				directionSymbol = 'd';
-		//				break;
-		//			}
-		//			saveFile << directionSymbol << " ";
-		//
-		//			break; // Break out of search, not output array
-		//		}
-		//	}
-		//}
+			Wire* newWire = nullptr;
+			for (Wire* wire : transistors[outputTransistorIndex]->inputs)
+			{
+				if (wire->inTransistor == transistor) // Searching for self as input to wire
+				{
+					newWire = wire;
+					wire->outTransistor = transistor;
+					break;
+				}
+			}
+			if (!newWire) // If an existing wire was not found, create one.
+			{
+				newWire = new Wire();
+				newWire->inTransistor = transistor;
+				newWire->outTransistor = transistors[outputTransistorIndex];
+				transistor->inputs.push_back(newWire);
+			}
+
+			char wireDirectionSymbol;
+			loadFile >> wireDirectionSymbol;
+			switch (wireDirectionSymbol)
+			{
+			default: case 'x':
+				newWire->direction = WireDirection::XFirst;
+				break;
+			case 'y':
+				newWire->direction = WireDirection::YFirst;
+				break;
+			case 'd':
+				newWire->direction = WireDirection::Direct;
+				break;
+			}
+		}
 	}
 
 	loadFile.close();
