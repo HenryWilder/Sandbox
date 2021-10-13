f504259bbbbd3130e92a9a3afd4fd5c0162ab64c
Amy <henrythepony@gmail.com>
2021-10-12 20:29:17 -0400

    Greatly improved selection and UI

--- RaylibSandbox/Spreadsheeter/Source.cpp
+++ RaylibSandbox/Spreadsheeter/Source.cpp
@@ -6,10 +6,10 @@
 
 int Min(int a, int b)
 {
-    if (a > b)
-        return b;
-    else
+    if (a < b)
         return a;
+    else
+        return b;
 }
 int Max(int a, int b)
 {
@@ -78,6 +78,8 @@ constexpr Color g_palette[] =
 #define PURPLE          g_palette[17]
 #define MAGENTA         g_palette[18]
 
+#define SELECTION_BLUE { 0x4a, 0x86, 0xe8, 32 }
+
 #define sign(x) (((x) > (decltype(x))(0)) - ((x) < (decltype(x))(0)))
 
 using Vector1 = float;
@@ -133,6 +135,154 @@ struct Range
 {
     Cell start, end;
 };
+// Does not say whether the rows/columns *match*
+bool IsRangeContiguous(Range a, Range b)
+{
+    return
+        a.end.x + 1 == b.start.x ||
+        b.end.x + 1 == a.start.x ||
+        a.end.y + 1 == b.start.y ||
+        b.end.y + 1 == a.start.y;
+}
+bool IsRangeMatchingR(Range a, Range b)
+{
+    return a.start.y == b.start.y && b.end.y == a.end.y;
+}
+bool IsRangeMatchingC(Range a, Range b)
+{
+    return a.start.x == b.start.x && b.end.x == a.end.x;
+}
+bool IsRangeOverlapping(Range a, Range b)
+{
+    Rectangle ra = { a.start.x, a.start.y, a.end.x - a.start.x, a.end.y - a.start.y };
+    Rectangle rb = { b.start.x, b.start.y, b.end.x - b.start.x, b.end.y - b.start.y };
+    return CheckCollisionRecs(ra, rb);
+}
+Range RangeFromCellPair(Cell a, Cell b)
+{
+    return
+        Range{
+            Cell{
+                Min(a.x, b.x),
+                Min(a.y, b.y)
+            },
+            Cell{
+                Max(a.x, b.x),
+                Max(a.y, b.y)
+            }
+        };
+}
+void SimplifyMultiRange(std::vector<Range>* ranges)
+{
+    /*******************************************************************************************************
+    *
+    *   RANGE COMBINATION
+    *
+    *   Compatible:
+    *
+    *     0. ROWS match and COLUMNS are contiguous. Order does not matter.
+    *
+    *         [A][A][B][B] or [B][B][A][A]
+    *         [A][A][B][B]    [B][B][A][A]
+    *
+    *         Result: Ranges can combine.
+    *
+    *         [A][A][A][A]
+    *         [A][A][A][A]
+    *
+    *
+    *     0. COLUMNS match and ROWS are contiguous. Order does not matter.
+    *
+    *         [A][A]    [B][B]
+    *         [A][A] or [B][B]
+    *         [B][B]    [A][A]
+    *         [B][B]    [A][A]
+    *
+    *         Result: Ranges can combine.
+    *
+    *         [A][A]
+    *         [A][A]
+    *         [A][A]
+    *         [A][A]
+    *
+    *
+    *   Incompatible:
+    *
+    *     1. Ranges are not contiguous either by row nor by column, whether their rows or columns match
+    *
+    *         [A][A]   [B][B]
+    *         [A][A]   [B][B]
+    *
+    *         Result: Do nothing.
+    *
+    *         [A][A]   [B][B]
+    *         [A][A]   [B][B]
+    *
+    *
+    *     2. Row AND column mismatch, even if partly contiguous
+    *
+    *         [A][A]
+    *         [A][A][B][B]
+    *               [B][B]
+    *
+    *         Result: Keep the two ranges separate.
+    *
+    *         [A][A]
+    *         [A][A][B][B]
+    *               [B][B]
+    *
+    *
+    *     3. Ranges overlap partially
+    *
+    *         [A][A]
+    *         [A](B)[B]
+    *            [B][B]
+    *
+    *         Result: Break the existing range apart.
+    *
+    *         [A][A]
+    *         [C][B][B]
+    *            [B][B]
+    *
+    *
+    *     4. Range does not go outside the bounds of the existing range
+    *
+    *         [A][A]                        [A][A][A]
+    *         (B)(B)  or  [A](B)(B)[A]  or  [A](B)[A]
+    *         (B)(B)      [A](B)(B)[A]      [A][A][A]
+    *         [A][A]
+    *
+    *         Result: Subtract the new range from the existing one.
+    *
+    *         [A][A]                        [A][A][A]
+    *                 or  [A]      [B]  or  [B]   [C]
+    *                     [A]      [B]      [D][D][D]
+    *         [B][B]
+    *
+    *******************************************************************************************************/
+
+    for (size_t i = 0; i < ranges->size(); ++i)
+    {
+        for (size_t j = 0; j < ranges->size(); ++j)
+        {
+            if (IsRangeContiguous((*ranges)[i], (*ranges)[j]) && (IsRangeMatchingR((*ranges)[i], (*ranges)[j]) || IsRangeMatchingC((*ranges)[i], (*ranges)[j])))
+            {
+                (*ranges)[i].start.x = Min((*ranges)[i].start.x, (*ranges)[j].start.x);
+                (*ranges)[i].start.y = Min((*ranges)[i].start.y, (*ranges)[j].start.y);
+                (*ranges)[i].end.x = Max((*ranges)[i].end.x, (*ranges)[j].end.x);
+                (*ranges)[i].end.y = Max((*ranges)[i].end.y, (*ranges)[j].end.y);
+            }
+        }
+    }
+}
+bool IsCellInRange(Cell cell, Range range)
+{
+    return
+        cell.x >= range.start.x &&
+        cell.x <= range.end.x &&
+        cell.y >= range.start.y &&
+        cell.y <= range.end.y;
+}
 std::vector<Range> g_selection = {};
 
 int PreviousRowHeight(int row)
@@ -177,6 +327,20 @@ void HighlightRange(Range range, Color color)
     int startY = PreviousRowHeight(range.start.y);
     DrawRectangle(startX, startY, g_ColumnWidths[range.end.x] - startX, g_RowHeights[range.end.y] - startY, color);
 }
+void OutlineRange(Range range, Color color, int thick = 1)
+{
+    int startX = PreviousColumnWidth(range.start.x);
+    int startY = PreviousRowHeight(range.start.y);
+    for (int i = 0; i < thick; ++i)
+    {
+        DrawRectangleLines(startX + i, startY + i, g_ColumnWidths[range.end.x] - startX - i * 2, g_RowHeights[range.end.y] - startY - i * 2, color);
+    }
+}
+void DrawHandle(Cell cell, Color color)
+{
+    DrawRectangle(g_ColumnWidths[cell.x] - 5, g_RowHeights[cell.y] - 5, 7, 7, WHITE);
+    DrawRectangle(g_ColumnWidths[cell.x] - 4, g_RowHeights[cell.y] - 4, 6, 6, color);
+}
 
 int main()
 {
@@ -206,7 +370,6 @@ int main()
     // -1 for none
     int cell_x = -1;
     int cell_y = -1;
-    int drag_offset = 0;
     int drag_distance = 0;
 
     Cell startOfSelection;
@@ -218,22 +381,21 @@ int main()
         ******************************************/
 
         // Something must already be selected before selection can be eliminated
-        if (mouseMode == MouseMode::Dragging && IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+        if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
         {
-            // Column
-            if (cell_x != -1)
+            if (mouseMode == MouseMode::Dragging && region == Region::Edge)
             {
-                for (int i = cell_x; i < g_ColumnWidths.size(); ++i)
+                // Column
+                if (cell_x != -1)
                 {
-                    g_ColumnWidths[i] += drag_distance;
+                    for (int i = cell_x; i < g_ColumnWidths.size(); ++i)
+                        g_ColumnWidths[i] += drag_distance;
                 }
-            }
-            // Row
-            else
-            {
-                for (int i = cell_y; i < g_RowHeights.size(); ++i)
+                // Row
+                else
                 {
-                    g_RowHeights[i] += drag_distance;
+                    for (int i = cell_y; i < g_RowHeights.size(); ++i)
+                        g_RowHeights[i] += drag_distance;
                 }
             }
             mouseMode = MouseMode::None;
@@ -243,7 +405,6 @@ int main()
         if (mouseMode != MouseMode::Dragging)
         {
             region = Region::Outside;
-            cell_x = cell_y = -1;
 
             // Find column
             if (GetMouseX() > 20)
@@ -253,15 +414,14 @@ int main()
                     if (GetMouseX() <= g_ColumnWidths[i])
                     {
                         cell_x = i;
-                        if (GetMouseY() <= 20 && (GetMouseX() + 5) >= g_ColumnWidths[i])
-                        {
+                        if (GetMouseY() <= 20 && (GetMouseX() + 8) >= g_ColumnWidths[i])
                             region = Region::Edge;
-                            drag_offset = GetMouseX() - g_ColumnWidths[i];
-                        }
                         break;
                     }
                 }
             }
+            else
+                cell_x = -1;
 
             // Find row
             if (GetMouseY() > 20)
@@ -271,17 +431,16 @@ int main()
                     if (GetMouseY() <= g_RowHeights[i])
                     {
                         cell_y = i;
-                        if (GetMouseX() <= 20 && (GetMouseY() + 5) >= g_RowHeights[i])
-                        {
+                        if (GetMouseX() <= 20 && (GetMouseY() + 8) >= g_RowHeights[i])
                             region = Region::Edge;
-                            drag_offset = GetMouseY() - g_RowHeights[i];
-                        }
                         break;
                     }
                 }
             }
+            else
+                cell_y = -1;
 
-            if (cell_x >= 0 && cell_y >= 0)
+            if (cell_x != -1 == cell_y != -1)
                 region = Region::Cell;
 
             if (region == Region::Edge)
@@ -291,39 +450,63 @@ int main()
                 else
                     SetMouseCursor(MOUSE_CURSOR_RESIZE_NS);
             }
+            else if (!g_selection.empty())
+            {
+                Rectangle rec = { g_ColumnWidths[g_selection.back().end.x] - 5, g_RowHeights[g_selection.back().end.y] - 5, 7, 7 };
+                if (CheckCollisionPointRec(GetMousePosition(), rec))
+                    SetMouseCursor(MOUSE_CURSOR_CROSSHAIR);
+                else
+                    SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+            }
             else
                 SetMouseCursor(MOUSE_CURSOR_DEFAULT);
 
             if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
             {
-                if (!(IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT) || IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL)))
-                    g_selection.clear();
+                mouseMode = MouseMode::Dragging;
 
-                if (region == Region::Edge)
-                    mouseMode = MouseMode::Dragging;
-
-                else if (region == Region::Cell)
+                if (region == Region::Cell)
                 {
                     Cell cell = { cell_x, cell_y };
 
-                    if (!g_selection.empty() && (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT)))
+                    if (!g_selection.empty() && (IsKeyDown(KEY_LEFT_SHIFT) || IsKeyDown(KEY_RIGHT_SHIFT))) // Expand selection
                     {
-                        Range& range = g_selection.back();
-                        range.start.x = Min(cell_x, startOfSelection.x);
-                        range.end.x = Max(cell_x, startOfSelection.x);
-                        range.start.y = Min(cell_y, startOfSelection.y);
-                        range.end.y = Max(cell_y, startOfSelection.y);
+                        if (cell_x == -1 && cell_y == -1)
+                        {
+                            g_selection.back() = RangeFromCellPair({ 0, startOfSelection.y }, { (int)g_ColumnWidths.size() - 1, 0});
+                            mouseMode = MouseMode::Hovering;
+                        }
+                        if (cell_x == -1)
+                        {
+
+                        }
+                        else if (cell_y == -1)
+                        {
+
+                        }
+                        else
+                            g_selection.back() = RangeFromCellPair(startOfSelection, cell);
                     }
                     else
                     {
-                        startOfSelection = cell;
-
-                        if (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL))
+                        if (IsKeyDown(KEY_LEFT_CONTROL) || IsKeyDown(KEY_RIGHT_CONTROL)) // Add selection
                         {
-                            g_selection.push_back({ cell, cell }); // todo
-                        }
-                        else
+                            startOfSelection = cell;
                             g_selection.push_back({ cell, cell });
+                        }
+                        else // Replace selection
+                        {
+                            startOfSelection = cell;
+                            g_selection.clear();
+                            if (cell_x == -1 && cell_y == -1)
+                                g_selection.push_back({ { 0,0 }, { (int)g_ColumnWidths.size() - 1, (int)g_RowHeights.size() - 1 } });
+                            if (cell_x == -1)
+                                g_selection.push_back({ { 0,cell_y }, { (int)g_ColumnWidths.size() - 1, cell_y } });
+                            else if (cell_y == -1)
+                                g_selection.push_back({ { cell_x,0 }, { cell_x, (int)g_RowHeights.size() - 1 } });
+                            else
+                                g_selection.push_back({ cell, cell });
+                        }
                     }
                 }
             }
@@ -350,6 +533,40 @@ int main()
                     
                 }
             }
+            else if (region == Region::Cell)
+            {
+                int temp_x, temp_y;
+                temp_x = temp_y = -1;
+
+                // Find column
+                if (GetMouseX() > 20)
+                {
+                    for (int i = 0; i < g_ColumnWidths.size(); ++i)
+                    {
+                        temp_x = i;
+                        if (GetMouseX() <= g_ColumnWidths[i])
+                            break;
+                    }
+                }
+
+                // Find row
+                if (GetMouseY() > 20)
+                {
+                    for (int i = 0; i < g_RowHeights.size(); ++i)
+                    {
+                        temp_y = i;
+                        if (GetMouseY() <= g_RowHeights[i])
+                            break;
+                    }
+                }
+
+                if (cell_x != temp_x || cell_y != temp_y)
+                {
+                    cell_x = temp_x;
+                    cell_y = temp_y;
+                    g_selection.back() = RangeFromCellPair(startOfSelection, { cell_x, cell_y });
+                }
+            }
         }
 
         /******************************************
@@ -360,9 +577,12 @@ int main()
 
             ClearBackground(WHITE);
 
-            for (size_t i = 0; i < g_selection.size(); ++i)
+            if (!(g_selection.size() == 1 && g_selection.back().start.x == g_selection.back().end.x && g_selection.back().start.y == g_selection.back().end.y))
             {
-                HighlightRange(g_selection[i], CORNFLOWER_BLUE);
+                for (size_t i = 0; i < g_selection.size(); ++i)
+                {
+                    HighlightRange(g_selection[i], SELECTION_BLUE);
+                }
             }
 
             DrawRectangle(0, 0, 20, g_RowHeights.back(), RAYWHITE);
@@ -399,10 +619,22 @@ int main()
                         DrawRectangle(0, g_RowHeights[cell_y] - 4, 20, 5, CORNFLOWER_BLUE);
                 }
             }
-            //else if (region == Region::Cell)
-            //{
-            //    DrawRectangle(PreviousColumnWidth(cell_x), PreviousRowHeight(cell_y), CellWidth(cell_x), CellHeight(cell_y), CORNFLOWER_BLUE);
-            //}
+
+            for (size_t i = 0; i < g_selection.size() - (mouseMode == MouseMode::Dragging ? 1 : 0); ++i)
+            {
+                OutlineRange(g_selection[i], CORNFLOWER_BLUE);
+            }
+            if (!g_selection.empty())
+            {
+                Cell startingCell = { (startOfSelection.x != -1 ? startOfSelection.x : 0), (startOfSelection.y != -1 ? startOfSelection.y : 0) };
+                OutlineRange({ startingCell, startingCell }, CORNFLOWER_BLUE, 2);
+                if (mouseMode != MouseMode::Dragging)
+                DrawHandle(g_selection.back().end, CORNFLOWER_BLUE);
+            }
+
+
+            if (!g_selection.empty())
+                DrawText(TextFormat("$%c$%i:$%c$%i",'A' + g_selection.back().start.x, g_selection.back().start.y, 'A' + g_selection.back().end.x, g_selection.back().end.y),0,0,8,RED);
 
         } EndDrawing();
     }
