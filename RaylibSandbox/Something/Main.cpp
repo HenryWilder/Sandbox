cfb34694001cef6dd9fdc8585653a2cea5db4a39
Amy <henrythepony@gmail.com>
2022-02-26 13:06:50 -0500

    Added draggable button

--- RaylibSandbox/Something/Main.cpp
+++ RaylibSandbox/Something/Main.cpp
@@ -69,26 +69,31 @@ private:
     }
 
 private:
+    std::string m_displayName;
     Rectangle m_rect;
     bool m_isToggle;
     State m_state;
-    std::string m_displayName;
+    bool m_callbackDirty;
 
 public:
     static constexpr bool Type_Toggle = true;
     static constexpr bool Type_Hold = false;
 
     Button(const std::string displayName, float x, float y, float width, float height, bool isToggle, bool startActive, bool startDisabled) :
+        m_displayName(displayName),
         m_rect{ x, y, width, height },
         m_isToggle(isToggle),
         m_state((State)(((unsigned char)State::ACTIVE * (unsigned char)startActive) | ((unsigned char)State::DISABLED * (unsigned char)startDisabled))),
-        m_displayName(displayName) {}
+        m_callbackDirty(false)
+    {}
 
     Button(const std::string displayName, Rectangle rect, bool isToggle, bool startActive, bool startDisabled) :
+        m_displayName(displayName),
         m_rect(rect),
         m_isToggle(isToggle),
         m_state((State)(((unsigned char)State::ACTIVE* (unsigned char)startActive) | ((unsigned char)State::DISABLED * (unsigned char)startDisabled))),
-        m_displayName(displayName) {}
+        m_callbackDirty(false)
+    {}
 
     Rectangle GetRect() const
     {
@@ -147,6 +152,19 @@ public:
     {
         m_displayName = displayName;
     }
+
+    bool IsUnhandled() const
+    {
+        return m_callbackDirty;
+    }
+    void MarkUnhandled()
+    {
+        m_callbackDirty = true;
+    }
+    void MarkHandled()
+    {
+        m_callbackDirty = true;
+    }
 };
 Button::State operator|(Button::State lValue, Button::State rValue)
 {
@@ -165,9 +183,9 @@ int main()
     ******************************************/
 
     std::vector<Button> buttons = {
-        { "Hold button", 20, 20, 60, 20, Button::Type_Hold, false, false },
-        { "Toggle button", 20, 50, 60, 20, Button::Type_Toggle, false, false },
-        { "Draggable button", 20, 80, 60, 20, Button::Type_Hold, false, false },
+        { "Hold", 20, 20, 60, 20, Button::Type_Hold, false, false },
+        { "Toggle", 20, 50, 60, 20, Button::Type_Toggle, false, false },
+        { "Draggable", 20, 80, 60, 20, Button::Type_Hold, false, false },
     };
 
     while (!WindowShouldClose())
@@ -176,7 +194,42 @@ int main()
         *   Simulate frame and update variables   *
         ******************************************/
 
-        // @TODO: simulate frame
+        Vector2 cursor = GetMousePosition();
+
+        for (Button& button : buttons)
+        {
+            bool hovered = button.IsOverlapping(cursor);
+            button.SetHovered(hovered);
+            if (hovered && button.IsToggle() && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
+                button.MarkUnhandled();
+                button.SetActive(!button.IsActive());
+            }
+            else if (!button.IsToggle())
+            {
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && hovered)
+                {
+                    button.SetActive(true);
+                    button.MarkUnhandled();
+                }
+                else if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+                    button.SetActive(false);
+            }
+        }
+
+        if (buttons[2].IsActive())
+        {
+            Rectangle rec = buttons[2].GetRect();
+            rec.y = Clamp(GetMousePosition().y - rec.height * 0.5f, 80.0f, 280.0f);
+
+            buttons[2].SetRect(rec);
+        }
+
+        // Handle
+        for (Button& button : buttons)
+        {
+            button.MarkHandled();
+        }
 
         /******************************************
         *   Draw the frame                        *
@@ -186,10 +239,30 @@ int main()
 
             ClearBackground(BLACK);
 
-            Vector2 cursor = GetMousePosition();
-            for (Button& button : buttons)
+            for (const Button& button : buttons)
             {
-                DrawRectangleRec(button.GetRect(), button.IsOverlapping(cursor) ? SKYBLUE : BLUE);
+                Color color;
+                if (button.IsDisabled())
+                    color = GRAY;
+                else
+                {
+                    if (button.IsHovered())
+                    {
+                        if (button.IsActive())
+                            color = WHITE;
+                        else
+                            color = BLUE;
+                    }
+                    else
+                    {
+                        if (button.IsActive())
+                            color = SKYBLUE;
+                        else
+                            color = DARKBLUE;
+                    }
+                }
+                DrawRectangleRec(button.GetRect(), color);
+                DrawText(button.GetDisplayName().c_str(), button.GetRect().x + 2, button.GetRect().y + 2, 8, BLACK);
             }
 
         } EndDrawing();
