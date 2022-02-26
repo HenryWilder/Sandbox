9440c80269a90ffe2447547665cdd05e2e114e51
Amy <henrythepony@gmail.com>
2022-02-26 18:09:31 -0500

    Added menu screen

--- RaylibSandbox/Something/Main.cpp
+++ RaylibSandbox/Something/Main.cpp
@@ -249,49 +249,217 @@ struct RadioButtonHandler
     }
 };
 
-int main()
+class UIHandler
 {
-    int windowWidth = 1280;
-    int windowHeight = 720;
-    InitWindow(windowWidth, windowHeight, "My Raylib Program");
-    SetTargetFPS(60);
+private:
+    UIHandler() {}
 
-    /******************************************
-    *   Load textures, shaders, and meshes    *
-    ******************************************/
+    std::vector<Button*> m_buttons;
+    std::vector<RadioButtonHandler*> m_groups;
 
-    std::vector<RadioButtonHandler> radios = {
-        {}
-    };
+public:
+    static UIHandler& Global()
+    {
+        static UIHandler object;
+        return object;
+    }
 
-    std::vector<Button> buttons = {
-        { "Hold", "Stays active only while the mouse is pressed.",
-            20, 20, 60, 20, Button::Type_Hold, false, false },
-        { "Toggle", "Changes states when the mouse is pressed.",
-            20, 50, 60, 20, Button::Type_Toggle, false, false },
-        { "DragV", "Shows the ability to drag a button while it is held.",
-            20, 80, 60, 20, Button::Type_Hold, false, false },
-        { "DragH", "Shows the ability to drag a button while it is held.",
-            20, 110, 60, 20, Button::Type_Hold, false, false },
+    const std::vector<Button*>& GetButtons() const
+    {
+        return m_buttons;
+    }
 
-        { "A", "Only one of these two can be active at a time.",
-            90, 20, 20, 20, Button::Type_Toggle, false, false, &(radios[0]) },
-        { "B", "Only one of these two can be active at a time.",
-            90, 50, 20, 20, Button::Type_Toggle, false, false, &(radios[0]) },
-        { "C'", "Only one of these two can be active at a time. Also, this one moves with the one on its left!",
-            90, 80, 20, 20, Button::Type_Toggle, false, false, &(radios[0]) },
-    };
+    // How many buttons to reserve space for (accounts for existing space, you don't need to add that)
+    void Expect(size_t newButtons)
+    {
+        m_buttons.reserve(m_buttons.size() + newButtons);
+    }
 
-    radios[0].m_children.reserve(3);
-    radios[0].m_children.push_back(&(buttons[4]));
-    radios[0].m_children.push_back(&(buttons[5]));
-    radios[0].m_children.push_back(&(buttons[6]));
+    // For when you're certain the button being added is new
+    void AddButton_New(Button* button)
+    {
+        if (!!button)
+            m_buttons.push_back(button);
+    }
+    void AddButton(Button* button)
+    {
+        if (!!button && std::find(m_buttons.begin(), m_buttons.end(), button) == m_buttons.end())
+            m_buttons.push_back(button);
+    }
+
+    // All buttons being supplied are already in the handler
+    void CreateButtonGroup_FromExisting(RadioButtonHandler* group, const std::vector<Button*>& buttons)
+    {
+        if (!group) return;
+        m_groups.push_back(group);
+        m_groups.back()->m_children.reserve(buttons.size());
+        for (Button* button : buttons)
+        {
+            button->SetGroup(m_groups.back());
+            m_groups.back()->m_children.push_back(button);
+        }
+    }
+    // All buttons being supplied are NOT already in the handler
+    void CreateButtonGroup_FromNew(RadioButtonHandler* group, const std::vector<Button*>& buttons)
+    {
+        if (!group) return;
+        m_groups.push_back(group);
+        m_groups.back()->m_children.reserve(buttons.size());
+        for (Button* button : buttons)
+        {
+            AddButton_New(button);
+            button->SetGroup(m_groups.back());
+            m_groups.back()->m_children.push_back(button);
+        }
+    }
+    void CreateButtonGroup(RadioButtonHandler* group, const std::vector<Button*>& buttons)
+    {
+        if (!group) return;
+        m_groups.push_back(group);
+        m_groups.back()->m_children.reserve(buttons.size());
+        for (Button* button : buttons)
+        {
+            AddButton(button);
+            button->SetGroup(m_groups.back());
+            m_groups.back()->m_children.push_back(button);
+        }
+    }
+
+    void UpdateButtons()
+    {
+        // Cleanup from last frame
+        for (Button* button : m_buttons)
+        {
+            button->ResetChanged();
+        }
+
+        // Find state
+        for (Button* button : m_buttons)
+        {
+            bool hovered = button->IsOverlapping(GetMousePosition());
+            button->SetHovered(hovered);
+            if (hovered && button->IsToggle() && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
+                if (!!button->GetGroup())
+                {
+                    button->GetGroup()->Activate(button);
+                }
+                else
+                {
+                    button->MarkChanged();
+                    button->SetActive(!button->IsActive());
+                }
+            }
+            else if (!button->IsToggle())
+            {
+                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && hovered)
+                {
+                    button->MarkChanged();
+                    button->SetActive(true);
+                }
+                else if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+                {
+                    button->MarkChanged();
+                    button->SetActive(false);
+                }
+            }
+        }
+
+        {
+            bool hovering = false;
+            for (const Button* button : m_buttons)
+            {
+                if (button->IsHovered())
+                {
+                    hovering = true;
+                    break;
+                }
+            }
+
+            SetMouseCursor(hovering ? MOUSE_CURSOR_POINTING_HAND : MOUSE_CURSOR_DEFAULT);
+        }
+
+        // Highlight grouped buttons together
+        for (RadioButtonHandler* group : m_groups)
+        {
+            bool hovered = false;
+            for (Button* child : group->m_children)
+            {
+                if (child->IsHovered())
+                {
+                    hovered = true;
+                    break;
+                }
+            }
+            if (hovered)
+            {
+                for (Button* child : group->m_children)
+                {
+                    child->SetHovered(true);
+                }
+            }
+        }
+    }
+
+    void DrawButtons()
+    {
+        // Draw buttons
+        for (const Button* button : m_buttons)
+        {
+            Color color;
+
+            // Determine color of state
+            if (button->IsDisabled())
+                color = GRAY;
+            else
+            {
+                if (button->IsHovered())
+                {
+                    if (button->IsActive())
+                        color = WHITE;
+                    else
+                        color = BLUE;
+                }
+                else
+                {
+                    if (button->IsActive())
+                        color = SKYBLUE;
+                    else
+                        color = DARKBLUE;
+                }
+            }
+
+            DrawRectangleRec(button->GetRect(), color); // Rectangle
+            DrawText(button->GetDisplayName().c_str(), button->GetRect().x + 2, button->GetRect().y + 2, 8, BLACK); // Name
+        }
+    }
+
+    void Unload()
+    {
+        m_buttons.clear();
+        m_groups.clear();
+    }
+};
+
+
+// Returns true when program should exit
+bool MenuScreen()
+{
+    UIHandler::Global().Expect(2);
+
+    Button button_start("Start", "",
+        20, 20, 60, 20, Button::Type_Hold, false, false);
+    UIHandler::Global().AddButton_New(&button_start);
+
+    Button button_close("Close", "",
+        20, 50, 60, 20, Button::Type_Hold, false, false);
+    UIHandler::Global().AddButton_New(&button_close);
 
     constexpr float toolTipHoverTime = 0.5f;
     float timeSinceMouseMove = 0.0f;
     Vector2 cursor{ 0,0 };
 
-    while (!WindowShouldClose())
+    while (true)
     {
         /******************************************
         *   Simulate frame and update variables   *
@@ -305,95 +473,125 @@ int main()
 
         cursor = GetMousePosition();
 
-        // Find state
-        for (Button& button : buttons)
-        {
-            bool hovered = button.IsOverlapping(cursor);
-            button.SetHovered(hovered);
-            if (hovered && button.IsToggle() && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-            {
-                if (!!button.GetGroup())
-                {
-                    button.GetGroup()->Activate(&button);
-                }
-                else
-                {
-                    button.MarkChanged();
-                    button.SetActive(!button.IsActive());
-                }
-            }
-            else if (!button.IsToggle())
-            {
-                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && hovered)
-                {
-                    button.MarkChanged();
-                    button.SetActive(true);
-                }
-                else if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
-                {
-                    button.MarkChanged();
-                    button.SetActive(false);
-                }
-            }
-        }
+        UIHandler::Global().UpdateButtons();
 
-        {
-            bool hovering = false;
-            for (const Button& button : buttons)
+        if (button_start.IsActive())
+            break;
+
+        if (button_close.IsActive())
+            return true;
+
+        /******************************************
+        *   Draw the frame                        *
+        ******************************************/
+
+        BeginDrawing(); {
+
+            ClearBackground(BLACK);
+
+            UIHandler::Global().DrawButtons();
+
+            // Draw tooltip
+            if (timeSinceMouseMove > toolTipHoverTime)
             {
-                if (button.IsHovered())
+                for (const Button* button : UIHandler::Global().GetButtons())
                 {
-                    hovering = true;
-                    break;
+                    if (button->IsHovered())
+                    {
+                        Rectangle rec = { cursor.x, cursor.y + 20, 200, 200 };
+                        Vector2 size = MeasureTextEx(GetFontDefault(), button->GetToolTip().c_str(), 10, 1);
+                        float lines = (fmodf(size.x, rec.width - 4) / size.y);
+                        rec.height = (lines)*size.y + 4.0f;
+
+                        DrawRectangleRec(rec, WHITE);
+                        rec.x += 2;
+                        rec.y += 2;
+                        rec.width -= 4;
+                        rec.height -= 4;
+                        DrawTextRec(GetFontDefault(), button->GetToolTip().c_str(), rec, 10, 1, true, BLACK);
+
+                        break;
+                    }
                 }
             }
 
-            SetMouseCursor(hovering ? MOUSE_CURSOR_POINTING_HAND : MOUSE_CURSOR_DEFAULT);
-        }
+        } EndDrawing();
 
-        // Highlight grouped buttons together
-        for (RadioButtonHandler& radio : radios)
-        {
-            bool hovered = false;
-            for (Button* child : radio.m_children)
-            {
-                if (child->IsHovered())
-                {
-                    hovered = true;
-                    break;
-                }
-            }
-            if (hovered)
-            {
-                for (Button* child : radio.m_children)
-                {
-                    child->SetHovered(true);
-                }
-            }
-        }
+        if (WindowShouldClose())
+            return true;
+    }
+
+    UIHandler::Global().Unload();
+    return false;
+}
+
+
+// Returns true when program should exit
+bool GameScreen()
+{
+    UIHandler::Global().Expect(7);
+
+    Button button_Hold("Hold", "Stays active only while the mouse is pressed.",
+        20, 20, 60, 20, Button::Type_Hold, false, false);
+    UIHandler::Global().AddButton_New(&button_Hold);
+
+    Button button_Toggle("Toggle", "Changes states when the mouse is pressed.",
+        20, 50, 60, 20, Button::Type_Toggle, false, false);
+    UIHandler::Global().AddButton_New(&button_Toggle);
+
+    Button button_DragV("DragV", "Shows the ability to drag a button while it is held.",
+        20, 80, 60, 20, Button::Type_Hold, false, false);
+    UIHandler::Global().AddButton_New(&button_DragV);
+
+    Button button_DragH("DragH", "Shows the ability to drag a button while it is held.",
+        20, 110, 60, 20, Button::Type_Hold, false, false);
+    UIHandler::Global().AddButton_New(&button_DragH);
+
+    RadioButtonHandler group0;
+    Button button_A("A", "Only one of these two can be active at a time.",
+        90, 20, 20, 20, Button::Type_Toggle, false, false);
+    Button button_B("B", "Only one of these two can be active at a time.",
+        90, 50, 20, 20, Button::Type_Toggle, false, false);
+    Button button_C("C", "Only one of these two can be active at a time. Also, this one moves with the one on its left!",
+        90, 80, 20, 20, Button::Type_Toggle, false, false);
+    UIHandler::Global().CreateButtonGroup_FromNew(&group0, { &button_A, &button_B, &button_C });
+
+    constexpr float toolTipHoverTime = 0.5f;
+    float timeSinceMouseMove = 0.0f;
+    Vector2 cursor{ 0,0 };
+
+    while (true)
+    {
+        /******************************************
+        *   Simulate frame and update variables   *
+        ******************************************/
+
+        // Update mouse hold still time
+        if (cursor.x != GetMousePosition().x || cursor.y != GetMousePosition().y || IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+            timeSinceMouseMove = 0.0f;
+        else if (timeSinceMouseMove <= toolTipHoverTime) // I am irrationally afraid of floating point imprecision
+            timeSinceMouseMove += GetFrameTime();
+
+        cursor = GetMousePosition();
+
+        UIHandler::Global().UpdateButtons();
 
         // Handle 3rd button dragging
-        if (buttons[2].IsActive())
+        if (button_DragV.IsActive())
         {
-            float y = Clamp(cursor.y - buttons[2].GetHeight() * 0.5f, 80.0f, 280.0f);
+            float y = Clamp(cursor.y - button_DragV.GetHeight() * 0.5f, 80.0f, 280.0f);
 
-            buttons[2].SetY(y);
-            buttons[3].SetY(y + 30);
-            buttons[6].SetY(y);
+            button_DragV.SetY(y);
+            button_DragH.SetY(y + 30);
+            button_C.SetY(y);
         }
         // Handle 3rd button dragging
-        if (buttons[3].IsActive())
+        if (button_DragH.IsActive())
         {
-            float x = Clamp(cursor.x - buttons[3].GetWidth() * 0.5f, 20.0f, 220.0f);
+            float x = Clamp(cursor.x - button_DragH.GetWidth() * 0.5f, 20.0f, 220.0f);
 
-            buttons[3].SetX(x);
-            buttons[6].SetX(x + 70);
-        }
-
-        // Mark handled
-        for (Button& button : buttons)
-        {
-            button.ResetChanged();
+            button_DragH.SetX(x);
+            button_C.SetX(x + 70);
         }
 
         /******************************************
@@ -404,54 +602,26 @@ int main()
 
             ClearBackground(BLACK);
 
-            // Draw buttons
-            for (const Button& button : buttons)
-            {
-                Color color;
-
-                // Determine color of state
-                if (button.IsDisabled())
-                    color = GRAY;
-                else
-                {
-                    if (button.IsHovered())
-                    {
-                        if (button.IsActive())
-                            color = WHITE;
-                        else
-                            color = BLUE;
-                    }
-                    else
-                    {
-                        if (button.IsActive())
-                            color = SKYBLUE;
-                        else
-                            color = DARKBLUE;
-                    }
-                }
-
-                DrawRectangleRec(button.GetRect(), color); // Rectangle
-                DrawText(button.GetDisplayName().c_str(), button.GetRect().x + 2, button.GetRect().y + 2, 8, BLACK); // Name
-            }
+            UIHandler::Global().DrawButtons();
 
             // Draw tooltip
             if (timeSinceMouseMove > toolTipHoverTime)
             {
-                for (const Button& button : buttons)
+                for (const Button* button : UIHandler::Global().GetButtons())
                 {
-                    if (button.IsHovered())
+                    if (button->IsHovered())
                     {
                         Rectangle rec = { cursor.x, cursor.y + 20, 200, 200 };
-                        Vector2 size = MeasureTextEx(GetFontDefault(), button.GetToolTip().c_str(), 10, 1);
+                        Vector2 size = MeasureTextEx(GetFontDefault(), button->GetToolTip().c_str(), 10, 1);
                         float lines = (fmodf(size.x, rec.width - 4) / size.y);
-                        rec.height = (lines) * size.y + 4.0f;
+                        rec.height = (lines)*size.y + 4.0f;
 
                         DrawRectangleRec(rec, WHITE);
                         rec.x += 2;
                         rec.y += 2;
                         rec.width -= 4;
                         rec.height -= 4;
-                        DrawTextRec(GetFontDefault(), button.GetToolTip().c_str(), rec, 10, 1, true, BLACK);
+                        DrawTextRec(GetFontDefault(), button->GetToolTip().c_str(), rec, 10, 1, true, BLACK);
 
                         break;
                     }
@@ -459,13 +629,42 @@ int main()
             }
 
         } EndDrawing();
+
+        if (WindowShouldClose())
+            return true;
     }
 
+    UIHandler::Global().Unload();
+    return true;
+}
+
+
+int main()
+{
+    int windowWidth = 1280;
+    int windowHeight = 720;
+    InitWindow(windowWidth, windowHeight, "My Raylib Program");
+    SetTargetFPS(60);
+
     /******************************************
-    *   Unload and free memory                *
+    *   Load textures, shaders, and meshes    *
     ******************************************/
 
-    // @TODO: Unload variables
+    enum class GameMode
+    {
+        Menu,
+        Game,
+    } mode = GameMode::Menu;
+
+    bool requestedClose = false;
+    while (!requestedClose)
+    {
+        switch (mode)
+        {
+        case GameMode::Menu: requestedClose = MenuScreen(); mode = GameMode::Game; break;
+        case GameMode::Game: requestedClose = GameScreen(); mode = GameMode::Menu; break;
+        }
+    }
 
     CloseWindow();
 
